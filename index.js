28f681aa0aa2dd8c14e242d454c37e204667f6ab
AC De Leon <acfromspace.deleon@gmail.com>
2020-01-02 14:16:39 -0800

    [+] final 🌱

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 module.exports = function weeb(string) {
+  var translations = {
+    no: "IIE",
+    plan: 'keikaku (Translator\'s note: "keikaku" means "plan")',
+    sorry: "GOMENASORRY",
+    what: "NANI",
+    yes: "HAI",
+  };
   if (typeof string !== "string")
     throw new TypeError("これは文字列ではありません!");
-  return string.replace("what", "NANI");
+  return string.replace(/no|plan|sorry|what|yes/gi, function(matched) {
+    return translations[matched];
+  });
 };
